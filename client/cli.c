92f3e54f66d209588cc59da8168285ff9fa600fa
CorodescuMihnea <corodescu.mihnea@gmail.com>
2019-01-17 09:34:50 +0200

    Client can now send a command to the server

--- client/cli.c
+++ client/cli.c
@@ -8,78 +8,70 @@
 #include <netdb.h>
 #include <string.h>
 
-/* codul de eroare returnat de anumite apeluri */
 extern int errno;
 
-/* portul de conectare la server*/
 int port;
 
 int main (int argc, char *argv[])
 {
-  int sd;			// descriptorul de socket
-  struct sockaddr_in server;	// structura folosita pentru conectare 
-  		// mesajul trimis
+  int sd;			
+  struct sockaddr_in server;	
   int nr=0;
-  char buf[10];
 
-  /* exista toate argumentele in linia de comanda? */
+
   if (argc != 3)
     {
-      printf ("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+      printf ("Syntax: %s <server_addr> <port>\n", argv[0]);
       return -1;
     }
 
-  /* stabilim portul */
+
   port = atoi (argv[2]);
 
-  /* cream socketul */
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
     {
-      perror ("Eroare la socket().\n");
+      perror ("Error at socket().\n");
       return errno;
     }
 
-  /* umplem structura folosita pentru realizarea conexiunii cu serverul */
-  /* familia socket-ului */
+
   server.sin_family = AF_INET;
-  /* adresa IP a serverului */
   server.sin_addr.s_addr = inet_addr(argv[1]);
-  /* portul de conectare */
   server.sin_port = htons (port);
   
-  /* ne conectam la server */
   if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
     {
-      perror ("[client]Eroare la connect().\n");
+      perror ("[client]Error at connect().\n");
       return errno;
     }
 
-  /* citirea mesajului */
-  printf ("[client]Introduceti un numar: ");
+  printf ("[client]Type a command: ");
   fflush (stdout);
-  read (0, buf, sizeof(buf));
-  nr=atoi(buf);
-  //scanf("%d",&nr);
+
+  char cmd[128];
+  int ret;
+  memset(cmd, 0, sizeof(cmd));
+  ret = read (0, cmd, sizeof(cmd) - 1);
+  cmd[ret] = '\0';
   
-  printf("[client] Am citit %d\n",nr);
+  printf("Preparing to write %s to server\n", cmd);
+  fflush(stdout);
 
-  /* trimiterea mesajului la server */
-  if (write (sd,&nr,sizeof(int)) <= 0)
+  if (write (sd, cmd, sizeof(cmd)) <= 0)
     {
-      perror ("[client]Eroare la write() spre server.\n");
+      perror ("[client]Error at write() to server.\n");
       return errno;
     }
 
-  /* citirea raspunsului dat de server 
-     (apel blocant pina cind serverul raspunde) */
-  if (read (sd, &nr,sizeof(int)) < 0)
+  char resp[20];
+  if (ret = read (sd, resp, sizeof(resp) - 1) < 0)
     {
-      perror ("[client]Eroare la read() de la server.\n");
+      perror ("[client]Error at read() from server.\n");
       return errno;
     }
-  /* afisam mesajul primit */
-  printf ("[client]Mesajul primit este: %d\n", nr);
+  resp[ret] = '\0';
 
-  /* inchidem conexiunea, am terminat */
+  printf ("[client]Server response is: %s\n", resp);
+
   close (sd);
 }
